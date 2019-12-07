1bd097e989850f85494e404d399a310571235f30
larycoder <lenhuchuhiep99@gmail.com>
2019-12-07 15:40:36 +0700

    LH    : Add FILEHandler to Server

--- Server/REST-Service/APIHandler/GETHandlerAPI.py
+++ Server/REST-Service/APIHandler/GETHandlerAPI.py
@@ -1,11 +1,11 @@
+from FileHandler import readFile
+from FileHandler import root
+
 # Predefine API
 Status_pair = {
   1:200,
   -1:404
 }
-
-# root directory of WEB server
-root = "../../Client"
 
 class API:
     """ A class to get request api and response correct data"""
@@ -14,7 +14,7 @@ class API:
     DBConnection = None
 
     # Attribute hold status of api
-    Status = 1
+    Status = -1
 
     # Buffer keep response message
     ResponseBuf = "Sorry"
@@ -22,35 +22,14 @@ class API:
     def __init__(self, API = '/', Connection = None):
       self.DBConnection = Connection
       if API == "/":
-        self.Status = self.readFile(root + "/Login/Login.html")
+        self.Status, self.ResponseBuf = readFile(root + "/Login/Login.html")
       else:
-        self.Status = self.readFile(root + API)
+        self.Status, self.ResponseBuf = readFile(root + API)
 
     def getResponse(self):
       return Status_pair[self.Status]
 
     def getData(self):
       return self.ResponseBuf
-    
-    def readFile(self, string):
-      # reference f for reading file
-      f = None
-
-      try:
-        f = open(string)
-        self.ResponseBuf = f.read()
-        f.close()
-        return 1
-      except IOError:
-        print("Error when processing file")
-        if not f == None:
-          print("Could not read file ", f)
-          f.close()
-        else:
-          print("File is not exit")
-        ErrorFile = open(root + "/error404.html")
-        self.ResponseBuf = ErrorFile.read()
-        ErrorFile.close()
-        return -1
 
     ### these are methods for handling each specific method ###
