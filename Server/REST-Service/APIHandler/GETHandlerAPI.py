c3d2f41faca14b1026baae9c6c5895ebdea36d48
larycoder <lenhuchuhiep99@gmail.com>
2019-12-07 14:38:15 +0700

    LH    : Add root file to Web Server

--- Server/REST-Service/APIHandler/GETHandlerAPI.py
+++ Server/REST-Service/APIHandler/GETHandlerAPI.py
@@ -1,14 +1,11 @@
 # Predefine API
-API_pair = {
-  '/':'get_login',
-  '/login':'get_login',
-  '/login/css':'get_login_css'
-}
-
 Status_pair = {
   1:200,
   -1:404
 }
+
+# root directory of WEB server
+root = "../../Client"
 
 class API:
     """ A class to get request api and response correct data"""
@@ -17,44 +14,43 @@ class API:
     DBConnection = None
 
     # Attribute hold status of api
-    Status = -1
+    Status = 1
 
     # Buffer keep response message
-    ResponseBuf = None
+    ResponseBuf = "Sorry"
 
     def __init__(self, API = '/', Connection = None):
       self.DBConnection = Connection
-
-
-      self.Status = getattr(self, API_pair[API])() # call method from string
+      if API == "/":
+        self.Status = self.readFile(root + "/Login/Login.html")
+      else:
+        self.Status = self.readFile(root + API)
 
     def getResponse(self):
       return Status_pair[self.Status]
 
     def getData(self):
       return self.ResponseBuf
+    
+    def readFile(self, string):
+      # reference f for reading file
+      f = None
+
+      try:
+        f = open(string)
+        self.ResponseBuf = f.read()
+        f.close()
+        return 1
+      except IOError:
+        print("Error when processing file")
+        if not f == None:
+          print("Could not read file ", f)
+          f.close()
+        else:
+          print("File is not exit")
+        ErrorFile = open(root + "/error404.html")
+        self.ResponseBuf = ErrorFile.read()
+        ErrorFile.close()
+        return -1
 
     ### these are methods for handling each specific method ###
-    
-    # GET login method
-    def get_login(self):
-      with open("../../Client/Login/Login.html", "r") as f:
-        try:
-          self.ResponseBuf = f.read()
-          f.close()
-          return 1
-        except IOError:
-          print("Could not read file", f)
-          f.close()
-          return -1
-
-    def get_login_css(self):
-      with open("../../Client/Login/login.css", "r") as f:
-        try:
-          self.ResponseBuf = f.read()
-          f.close()
-          return 1
-        except IOError:
-          print("Could not read file", f)
-          f.close()
-          return -1
