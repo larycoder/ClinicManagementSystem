73c1900779d81f8e6ea854b8fa2d95c74f05bbee
larycoder <lenhuchuhiep99@gmail.com>
2019-12-10 03:12:21 +0000

    LHDesk: [UPDATE] add api requet to js controller of client

--- Client/Login/login.js
+++ Client/Login/login.js
@@ -1,15 +1,30 @@
+function isJson(string) {
+    try {
+        JSON.parse(string);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 function validate(){
     var uname = document.getElementById("uname").value;
     var pass = document.getElementById("pass").value;
     let xmlRequest = new XMLHttpRequest();
     xmlRequest.onreadystatechange = function(){
+        if (isJson(this.responseText) == false) {
+            alert('Login fall');
+            return;
+        }
         let obj = JSON.parse(this.responseText);
-        var result = obj[uname];
-        if (result == pass){
+        var result = obj[ID];
+        if (result > 0 ){
             alert('Login successfully');
         }
-            
+        else {
+            alert('Login fall');
+        }   
     }
-    xmlRequest.open("GET","login.json");
+    xmlRequest.open("GET","/api/login?username=" + uname + "&password=" + pass);
     xmlRequest.send();
 }
