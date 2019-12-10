a0645d938b03f5d7c17df2ee82e9389bb2c8aac2
larycoder <lenhuchuhiep99@gmail.com>
2019-12-10 06:02:01 +0000

    LHDesk: update name file

---
+++ Server/REST-Service/APIHandler/Tool.py
@@ -0,0 +1,35 @@
+# function process file
+# root directory of WEB server
+import os
+
+root = os.path.dirname(os.path.realpath(__file__)) + "/../../../Client"
+
+## tool function ##
+def String2Json(data):
+  JsonFile = {} # dictionary to keep json object
+  values = data.split("&")
+  for value in values:
+    value = value.split("=")
+    JsonFile[value[0]] = value[1]
+  return JsonFile
+
+def readFile(String):
+      # reference f for reading file
+      f = None
+
+      try:
+        f = open(String)
+        Buff = f.read()
+        f.close()
+        return [1, Buff]
+      except IOError:
+        print("Error when processing file")
+        if not f == None:
+          print("Could not read file ", f)
+          f.close()
+        else:
+          print("File is not exit")
+        ErrorFile = open(root + "/error404.html")
+        Buff = ErrorFile.read()
+        ErrorFile.close()
+        return [-1, Buff]
