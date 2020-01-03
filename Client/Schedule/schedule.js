91259ea517aed44bf95c5b418fa3675a545faca3
larycoder <lenhuchuhiep99@gmail.com>
2020-01-03 11:11:13 +0700

    LH    : add list doctor option to schedule

--- Client/Schedule/schedule.js
+++ Client/Schedule/schedule.js
@@ -1,28 +1,40 @@
-
-$( function() {
-  $( "#datepicker" ).datepicker({
-    dateFormat: "yy-mm-dd",
-    minDate: 0,
-    maxDate: 14,
-  });
-  
-
-  $('#timepicker').timepicker({
-    timeFormat: 'HH:mm',
-    interval: 30,
-    minTime: '8:00am',
-    maxTime: '5:00pm',
-    dynamic: false,
-    dropdown: true,
-    scrollbar: true
-  });
-
-} );
-
-
-
-
-function test(){
-  alert(getStart() );
+// process with api
+function isJson(string) {
+  try {
+      JSON.parse(string);
+  } catch (e) {
+      return false;
+  }
+  return true;
 }
 
+function getUserID(){
+  let id = localStorage.getItem('Clinic-ID');
+  if(id !== null){
+      return id;
+  }
+  return -1;
+}
+
+function getListDoctor() {
+  let xmlRequest = new XMLHttpRequest();
+  xmlRequest.onreadystatechange = function(){
+    if (this.readyState == 4) {
+      if(xmlRequest.status == 200){
+        if(isJson(this.responseText)){
+          var list = JSON.parse(this.responseText);
+          var doctorOption = document.getElementById("listDoctor");
+          for(doctor in list){
+            let name = list[doctor].doctor_name;
+            let newDocOp = document.createElement("option");
+            newDocOp.value = doctor;
+            newDocOp.innerHTML = name;
+            doctorOption.appendChild(newDocOp);
+          }
+        }
+      }
+    }
+  }
+  xmlRequest.open("GET", "/api/doctorList?id=" + getUserID());
+  xmlRequest.send();
+}
